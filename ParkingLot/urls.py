34c66e2b638da5adfc738c6c646e101e723c4f0b
nidhu23 <nidhunanik@gmail.com>
2020-09-14 00:35:07 +0530

    [Nidhuna]Add: url pattern added for jwt token and registration app

--- ParkingLot/urls.py
+++ ParkingLot/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
-    path('',include('User.urls')),
+    path('', include('registration.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view()),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
 ]
