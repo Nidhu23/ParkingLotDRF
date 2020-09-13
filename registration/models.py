6945ff2aa4a4eadce7bc7ee58e38012405e6cfaf
nidhu23 <nidhunanik@gmail.com>
2020-09-14 00:37:51 +0530

    [Nidhuna]Add: User and role model class added

---
+++ registration/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLE_CHOICES = (("DR", "driver"), ("PL", "police"), ("SY", "security"),
+                ("LO", "lot_owner"))
+
+
+class Roles(models.Model):
+    role = models.CharField(primary_key=True,
+                            max_length=30,
+                            choices=ROLE_CHOICES)
+    charge = models.DecimalField(max_digits=8, decimal_places=4)
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    
