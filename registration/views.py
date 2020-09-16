37929450d8958e9e5491ebe588da35c87fe266c4
nidhu23 <nidhunanik@gmail.com>
2020-09-16 18:40:08 +0530

    [Nidhuna]Add: Login api view added

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .serializers import RegisterSerializer
 from rest_framework import generics, mixins
+from rest_framework.views import APIView
+from rest_framework import request
+from rest_framework.response import Response
+from .models import User
+from ParkingLot import settings
+import jwt
 
 
 # Create your views here.
@@ -9,3 +15,28 @@ class Register(generics.GenericAPIView, mixins.CreateModelMixin):
 
     def post(self, request):
         return self.create(request)
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if (username is None) or (password is None):
+            return Response("username and password required")
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response("user not registered,please register")
+        if (user is None):
+            return Response("username required")
+        if not user.check_password(password):
+            return Response("wrong password entered")
+        access_token_payload = {
+            'username': user.username,
+            'password': user.password
+        }
+
+        access_token = jwt.encode(access_token_payload, settings.SECRET_KEY)
+        
+        data = {"user": user.username, "token": access_token}
+        return Response(data)
