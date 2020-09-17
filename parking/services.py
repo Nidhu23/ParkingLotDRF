ab40d5ad7ce6b9e51aadddaf86bec64554a2e482
nidhu23 <nidhunanik@gmail.com>
2020-09-17 22:04:18 +0530

    [Nidhuna]Add: function to calculate total charge

---
+++ parking/services.py
@@ -0,0 +1,30 @@
+from django.utils import timezone
+from .models import UnPark
+
+
+def un_park(instance):
+    slot = instance.slot
+    vehicle_num = instance.vehicle_num
+    entry_time = instance.entry_time
+    exit_time = timezone.now()
+    charge = calc_charge(exit_time, instance)
+    un_parked = UnPark.objects.create(charge=charge,
+                                      slot=slot,
+                                      entry_time=entry_time,
+                                      exit_time=exit_time,
+                                      vehicle_num=vehicle_num)
+    un_parked.save()
+    instance.delete()
+
+
+def calc_charge(exit_time, instance):
+    total_parked_time = (exit_time - instance.entry_time)
+    total_hours = total_parked_time.total_seconds() / 3600
+    if total_hours < 1:
+        total_hours = 1
+    charges = total_hours * (float(instance.vehicle_type.charge) + float(
+        instance.park_type.charge) + float(instance.user.role.charge))
+    if instance.disabled:
+        DISCOUNT = 0.1
+        charges = charges - (charges * DISCOUNT)
+    return charges
