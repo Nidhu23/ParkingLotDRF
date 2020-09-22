e5ec5acbda00049b440bf7e04291bcb4caa7074e
nidhu23 <nidhunanik@gmail.com>
2020-09-22 19:18:53 +0530

    [Nidhuna]Delete: unnecessary app folder

--- registration/redis_setup.py
+++
@@ -1,5 +0,0 @@
-import redis
-
-
-def get_redis_instance():
-    return redis.Redis(host='localhost', port=6379, db=0)
--- registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from registration import views
-
-urlpatterns = [
-    path('register/', views.register),
-    path('login/',
-         views.Login().as_view()),
-    path('logout', views.logout)
-]
