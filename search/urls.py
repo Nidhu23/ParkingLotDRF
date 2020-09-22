b52bd1964be8a72305b66cf4a6f314d0674f7325
nidhu23 <nidhunanik@gmail.com>
2020-09-22 19:18:06 +0530

    [Nidhuna]Delete: unnecessary app folder

--- search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('searchbynum/', views.vehicle_num_search),
-    path('searchbytype/', views.vehicle_type_search),
-    path('searchbycolor/', views.vehicle_color_search)
-]
