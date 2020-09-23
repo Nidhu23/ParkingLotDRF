23d002f3f4866513ad400c217ae9e4b211333f9e
nidhu23 <nidhunanik@gmail.com>
2020-09-23 10:28:48 +0530

    [Nidhuna]Refactor: name of app registration changed to users

--- parking/serializers.py
+++ parking/serializers.py
@@ -6,8 +6,12 @@ class ParkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parking
         fields = [
-            'vehicle_num', 'vehicle_type', 'park_type', 'user', 'disabled'
+            'vehicle_num', 'vehicle_type', 'park_type', 'user', 'disabled',
+            'vehicle_color'
         ]
 
 
-
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parking
+        fields = ['vehicle_num', 'vehicle_color', 'vehicle_type', 'slot']
--- parking/urls.py
+++ parking/urls.py
@@ -3,4 +3,7 @@ from parking import views
 
 urlpatterns = [
     path('park-unpark/', views.Park.as_view()),
+    path('searchbynum/', views.vehicle_num_search),
+    path('searchbytype/', views.vehicle_type_search),
+    path('searchbycolor/', views.vehicle_color_search)
 ]
